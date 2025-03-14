883ceb34bcd17ab7d225a9b3a042d5f89176db18
Md Shakil Hossain <shakilshd23@gmail.com>
2025-03-15 01:51:09 +0600

    WhyChooseLocatel section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,14 @@ import Faq from "../components/Home/Faq";
 import Hero from "../components/Home/Hero";
 import InfoTable from "../components/Home/InfoTable";
 import LocatelGold from "../components/Home/LocatelGold";
+import WhyChooseLocatel from "../components/Home/WhyChooseLocatel"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <LocatelGold />
+      <WhyChooseLocatel />
       <InfoTable />
       <Faq />
       <AdditionalBenefit />
