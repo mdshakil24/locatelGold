5238930715ea901bc42e588eb2f862eb46ee555c
Md Shakil Hossain <shakilshd23@gmail.com>
2025-03-15 03:18:45 +0600

    ElegirLocatel Added and First Home page done

---
+++ src/components/Home/ElegirLocatel.jsx
@@ -0,0 +1,90 @@
+
+
+const ElegirLocatel = () => {
+    return (
+        <section className="elegir-locatel-area">
+            <div className="container mx-auto">
+
+                <div className="why-choose-text-content">
+                    <h2>¿Por qué elegir Locatel Gold?</h2>
+                    <p className="subtitle">Más que una membresía, un nuevo nivel de bienestar</p>
+                    <p className="description">
+                        Con Locatel Gold, accedes a un mundo de beneficios diseñados para cuidar tu salud y bienestar sin complicaciones. Desde consultas médicas ilimitadas hasta descuentos exclusivos en tiendas Locatel, todo está al alcance de tu mano. Afiliarte es rápido y sencillo, sigue estos pasos y empieza a disfrutar de una atención médica premium sin esperas:
+                    </p>
+                </div>
+
+            </div>
+
+            <div className="lg-plans-listing">
+                <div className="container">
+                    <div className="lg-plans-listing-content">
+
+                        <div className="single-lg-plans-listing">
+                            <span>1</span>
+                            <p>Elige tu plan: Trimestral, Semestral o Anual y accede a beneficios exclusivos.</p>
+                        </div>
+
+                        <div className="single-lg-plans-listing">
+                            <span>2</span>
+                            <p>Regístrate en línea y empieza a disfrutar de la mejor atención Premium.</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="info-top-area">
+                <div className="info-top-content">
+
+                    {/* single item */}
+                    <div className="single-info-top-content">
+                        <img src="/public/icons/tik.png" alt="tik" />
+                        <p>Descuentos exclusivos</p>
+                    </div>
+                    {/* single item */}
+
+                    {/* single item */}
+                    <div className="single-info-top-content">
+                        <img src="/public/icons/tik.png" alt="tik" />
+                        <p>Envios Expres</p>
+                    </div>
+                    {/* single item */}
+
+                    {/* single item */}
+                    <div className="single-info-top-content">
+                        <img src="/public/icons/tik.png" alt="tik" />
+                        <p>Telemedicina 24/7 ilimitada</p>
+                    </div>
+                    {/* single item */}
+                </div>
+
+                <div className="info-top-content">
+
+                    {/* single item */}
+                    <div className="single-info-top-content">
+                        <img src="/public/icons/tik.png" alt="tik" />
+                        <p>Alquiler de equipos médicos sin depósito</p>
+                    </div>
+                    {/* single item */}
+
+                    {/* single item */}
+                    <div className="single-info-top-content">
+                        <img src="/public/icons/tik.png" alt="tik" />
+                        <p>Asistencia del viajero </p>
+                    </div>
+                    {/* single item */}
+
+                    {/* single item */}
+                    <div className="single-info-top-content">
+                        <img src="/public/icons/tik.png" alt="tik" />
+                        <p>Acumulación de puntos</p>
+                    </div>
+                    {/* single item */}
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ElegirLocatel;
--- src/components/Home/InfoTable.jsx
+++ src/components/Home/InfoTable.jsx
@@ -1,51 +1,60 @@
 import React from "react";
+import ElegirLocatel from "./ElegirLocatel";
 
 export default function InfoTable() {
   return (
     <section className="info--table">
+
+    <ElegirLocatel />
+
       <div className="container mx-auto">
-        <table className="pricing-table">
-          <thead>
-            <tr>
-              <th>Plan</th>
-              <th>Precio regular</th>
-              <th>Precio especial</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Trimestral</td>
-              <td>$66,150 COP</td>
-              <td>xxxxxx</td>
-              <td>
-                <div className="button">
-                  <a href="#">¡Comprar membresía!</a>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Trimestral</td>
-              <td>$66,150 COP</td>
-              <td>xxxxxx</td>
-              <td>
-                <div className="button">
-                  <a href="#">¡Comprar membresía!</a>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td>Trimestral</td>
-              <td>$66,150 COP</td>
-              <td>xxxxxx</td>
-              <td>
-                <div className="button">
-                  <a href="#">¡Comprar membresía!</a>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+
+        {/* Info table content */}
+        <div className="info-table-content">
+          <table className="pricing-table">
+            <thead>
+              <tr>
+                <th>Plan</th>
+                <th>Precio regular</th>
+                <th>Precio especial</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Trimestral</td>
+                <td>$66,150 COP</td>
+                <td>xxxxxx</td>
+                <td>
+                  <div className="button">
+                    <a href="#">¡Comprar membresía!</a>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>Trimestral</td>
+                <td>$66,150 COP</td>
+                <td>xxxxxx</td>
+                <td>
+                  <div className="button">
+                    <a href="#">¡Comprar membresía!</a>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td>Trimestral</td>
+                <td>$66,150 COP</td>
+                <td>xxxxxx</td>
+                <td>
+                  <div className="button">
+                    <a href="#">¡Comprar membresía!</a>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p className="subtitle2">*Aplican condiciones y restricciones</p>
       </div>
     </section>
   );
